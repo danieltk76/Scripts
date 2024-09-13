629cbdf2dbc48c0f35dc49ee61a9f3af4cd3a8c1
danieltk76 <knight.daniel0612@gmail.com>
2024-09-13 09:26:04 -0400

    fixed to force admin privalges, will need rework to be totally anonymous

--- DiskCorruption.py
+++ DiskCorruption.py
@@ -1,15 +1,26 @@
+import ctypes
 import os
 import smtplib
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
+import sys
+
+# admin check, we do need admin to run the script
+def is_admin():
+    try:
+        return ctypes.windll.shell32.IsUserAnAdmin()
+    except:
+        return False
+
+
 
 # you could set these emails to one address so you would be emailing yourself
 # or you could setup a personal systems email so you have a diff email for each device
 
 def send_email(subject, body):
-    sender_email = "example@gmail.com"
-    receiver_email = "recieve@gmail.com"
-    password = "email_pass"
+    sender_email = "test@gmail.com"
+    receiver_email = "test@gmail.com"
+    password = "testpass"
 
     msg = MIMEMultipart()
     msg['From'] = sender_email
@@ -43,4 +54,8 @@ def run_chkdsk():
         print("No corruption detected")
 
 if __name__ == "__main__":
+    if not is_admin():
+        print("Script is not running as admin. Restarting with admin priv.")
+        ctypes.windll.shell32.ShellExecuteW(None, "runas", sys.executable, __file__, None, 1)
+
     run_chkdsk()
